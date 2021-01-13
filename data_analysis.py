96062e4720e07663cd3b8ebdf2e59d96d6ab40d4
pshivaji <67939704+pshivaji@users.noreply.github.com>
2021-01-13 11:38:06 +1000

    Update data_analysis.py

--- data_analysis.py
+++ data_analysis.py
@@ -20,13 +20,13 @@ def csv_json(i):
 def data_summary(j):
     df = pd.read_csv(j)
     fig, axes = plt.subplots(nrows=2, ncols=3 , figsize=(15,15))
-
     df['geo_country'].value_counts()[1:20].plot(ax=axes[0,0],title = "country",kind = "barh",color = "red")
     df['geo_region'].value_counts()[1:20].plot(ax=axes[0,1],title = "region", kind = "bar",color = "blue")
     df['geo_city'].value_counts()[1:20].plot(ax=axes[1,0],title = "city", kind = "bar")
     df['sku'].value_counts().plot(ax=axes[1,1],title = "sku",kind ="pie")
     df['is_limited_ad_tracking'].value_counts().plot(ax=axes[0,2],title = "is_limited_ad_tracking",kind = "pie")
     df['device_language'].value_counts()[1:20].plot(ax=axes[1,2],title = "language", kind = "barh",color = "violet")
+    fig.suptitle('Total categorized user_counts ',fontsize = 16)
     plt.show()
 
     
@@ -65,5 +65,3 @@ if __name__ == '__main__':
     k = input("convert to sql statements(y/n): ")
     if k == "y" :
         sql_insert(p)
-
-    
